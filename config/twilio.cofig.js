6be834ca62b2df97dbb5f70d9ea854ecabaf9189
VikashK21 <vikash21@navgurukul.org>
2022-08-12 11:08:17 +0530

    just made formal

--- config/twilio.cofig.js
+++ config/twilio.cofig.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    serviceID: process.env.SERVICE_ID,
-    accountSID: process.env.ACCOUTN_SID,
-    authToken: process.env.AUTH_TOKEN
+    serviceID: process.env.TWILIO_SERVICE_ID,
+    accountSID: process.env.TWILIO_ACCOUTN_SID,
+    authToken: process.env.TWILIO_AUTH_TOKEN
 }
