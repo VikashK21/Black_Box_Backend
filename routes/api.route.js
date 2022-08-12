7c89e115551fb359bba9d80bc43ec942e814dd77
VikashK21 <vikash21@navgurukul.org>
2022-08-12 13:43:30 +0530

    the addition of logout

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,17 +1,20 @@
 const router = require("express").Router();
 const Users = new (require("../controllers/users.controller"))();
+const {forLogout} = require("../auth/user.auth");
 
 router.get("/", async (req, res, next) => {
   res.send({
     message: "Ok api is working 🚀",
     login: "https://black-box-backend.herokuapp.com/api/login",
     signup: "https://black-box-backend.herokuapp.com/api/signup",
+    logout: "https://black-box-backend.herokuapp.com/api/logout",
   });
 });
 
 //Users:
-router.post("/signup", Users.signup);
-router.post("/login", Users.login);
+router.post("/signup", forLogout, Users.signup);
+router.post("/login", forLogout, Users.login);
+router.post("/logout", Users.logout);
 
 //Classes:
 
