bf5cd036583667c01370f9dccc5d1f2d823fc7b4
VikashK21 <vikash21@navgurukul.org>
2022-09-05 00:12:56 +0530

    the final...

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
