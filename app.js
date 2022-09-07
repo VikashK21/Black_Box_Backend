0d93c91fd33865d94d8fad7c569ce4dcbd7b4fb9
VikashK21 <vikash21@navgurukul.org>
2022-09-07 12:30:00 +0530

    the aws cren

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Remeber the errors can be from here also, when without .evn...
 require("dotenv").config();
-require("./auth/google.auth");
+// require("./auth/google.auth");
 // require("./auth/facebook.auth");
 //Modules : ) ...
 const express = require("express");
