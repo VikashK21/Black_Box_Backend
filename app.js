adea1b11494bc6e3627d10501b0244ab95e6fbfd
VikashK21 <vikash21@navgurukul.org>
2022-09-08 16:47:37 +0530

    the path

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const express = require("express");
 const createError = require("http-errors");
 const passport = require("passport");
 const session = require("express-session");
+const path = require("path");
 
 //Helping to understand APIs...
 // const swaggerJSDoc = require("swagger-jsdoc");
@@ -35,18 +36,20 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //Home page
-
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"))
+  })
+  // app.use(express.static("client/build"));
 }
 // app.get("/", async (req, res, next) => {
 //   res.send({
 //     message:
 //       "You are successfully standing on to the root page, please go to /api/users || /api/course page to view details.",
 //   });
-  // res.send(
-  //   '<a href="http://localhost:3001/api/signup/google">Authenticate with Google</a>'
-  // );
+// res.send(
+//   '<a href="http://localhost:3001/api/signup/google">Authenticate with Google</a>'
+// );
 // });
 
 //The APIs sections...
