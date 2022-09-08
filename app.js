8ad5df0ba44e1b5be209400480a262d7188f2951
VikashK21 <vikash21@navgurukul.org>
2022-09-08 11:48:04 +0530

    the port

--- app.js
+++ app.js
@@ -68,5 +68,5 @@ app.use((err, req, res, next) => {
 });
 
 //The listner...
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
