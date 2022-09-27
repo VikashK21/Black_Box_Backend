c469fedb0f581e65ad8f3aecb5e0761b42d79c59
VikashK21 <vikash21@navgurukul.org>
2022-09-27 16:42:00 +0530

    the changes firewall settings...

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ const cors = require("cors");
 const app = express();
 
 //Middlewares Section...
-app.use(cors());
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(
   session({
     secret: process.env.SECRET_KEY_TOKEN,
