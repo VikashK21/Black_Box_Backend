cb95463a78acc40a8f3f62f31bf70a23165f6f4a
VikashK21 <vikash21@navgurukul.org>
2022-09-28 18:43:59 +0530

    the deletion

--- client/src/Utils/PrivateRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-
-const PrivateRoutes = () => {
-  let token = localStorage.getItem("authTokens");
-  return token ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoutes;
