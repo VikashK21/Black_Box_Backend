45a14b87e8047ce9df1d7ae52eff5c4444aaa17e
VikashK21 <vikash21@navgurukul.org>
2022-09-28 15:23:29 +0530

    the origin error

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const cors = require("cors");
 const app = express();
 
 //Middlewares Section...
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
