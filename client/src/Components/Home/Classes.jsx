26d1fe43460c01f8590a66529be6b11c73f84790
VikashK21 <vikash21@navgurukul.org>
2022-09-28 15:01:59 +0530

    the length

--- client/src/Components/Home/Classes.jsx
+++ client/src/Components/Home/Classes.jsx
@@ -2,8 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import AuthContext from "../../Context/AuthContext";
-import ReadMoreReact from 'read-more-react';
-
+import ReadMoreReact from "read-more-react";
 
 // import HoverVideoPlayer from 'react-hover-video-player';
 // import Class2 from "../../Images/Classes/class2.jpg";
@@ -20,6 +19,7 @@ const Classes = () => {
 
   useEffect(() => {
     getCoursesList();
+    console.log(courseList);
   }, []);
 
   return (
@@ -34,67 +34,73 @@ const Classes = () => {
           </div>
 
           <Row className="mb-5 px-2 d-flex justify-content-center m-0 p-0">
-                {/* Map a function to display the classes list */}
-                {courseList &&
-                  courseList.slice(0,4).map((course) => {
-                    // console.log(course.images);
-                    const a = JSON.parse(course.images);
-                    return (
-                      <Col
-                        key={course.id}
-                        className="my-4 cp position-relative"
-                        style={{ minWidth: "320px", maxWidth: "320px" , minHeight: '400px' }}
-                      >
-                        <Link to={`/classes/join/${course.id}`}>
-                          <div onClick={goToTop} className="zoom">
-                            <div className="imgdiv">
-                              <img
-                                src={a[0]}
-                                alt="No images uploaded"
-                                className="classesimg p-0 m-0"
-                              />
-                              {/* <img src={course.images[0].secure_url} className="classesimg p-0 m-0" /> */}
-                            </div>
-                            <Row className="profile m-0 p-2 pt-4">
-                              <h6 className="gx classtitlee">{course.title}</h6>
-                              <p className="clsdesc"><ReadMoreReact
-                                    text={course.description}
-                                    min={150}
-                                    ideal={200}
-                                    max={500}
-                                    readMoreText=".. read more"
-                                  /></p>
+            {/* Map a function to display the classes list */}
+            {courseList.length > 0 &&
+              courseList.slice(0, 4).map((course) => {
+                // console.log(course.images);
+                const a = JSON.parse(course.images);
+                return (
+                  <Col
+                    key={course.id}
+                    className="my-4 cp position-relative"
+                    style={{
+                      minWidth: "320px",
+                      maxWidth: "320px",
+                      minHeight: "400px",
+                    }}
+                  >
+                    <Link to={`/classes/join/${course.id}`}>
+                      <div onClick={goToTop} className="zoom">
+                        <div className="imgdiv">
+                          <img
+                            src={a[0]}
+                            alt="No images uploaded"
+                            className="classesimg p-0 m-0"
+                          />
+                          {/* <img src={course.images[0].secure_url} className="classesimg p-0 m-0" /> */}
+                        </div>
+                        <Row className="profile m-0 p-2 pt-4">
+                          <h6 className="gx classtitlee">{course.title}</h6>
+                          <p className="clsdesc">
+                            <ReadMoreReact
+                              text={course.description}
+                              min={150}
+                              ideal={200}
+                              max={500}
+                              readMoreText=".. read more"
+                            />
+                          </p>
 
-                              <Col xs={8} className="p-2 pt-2 pb-0">
-                                <h6 className="gx tutorname">
-                                  {course.host_details.first_name}{" "}
-                                  {course.host_details.last_name}
-                                </h6>
-                                <p className="gl text-dark">Instructor</p>
-                              </Col>
-                              <div className="  clsfee p-2 pt-0">
-                                <div className="d-flex">
-                                  <h6 className="gx">
-                                    <span className="textgrey">FEE:</span> ₹
-                                    {course.price}
-                                    <span className="gl">/ person</span>
-                                  </h6>
-                                </div>
-                                <div className="d-flex">
-                                  <h6 className="gx">
-                                    <span className="textgrey"> Type:</span>{" "}
-                                    {course.duration_type}
-                                  </h6>
-                                </div>
-                              </div>
-                            </Row>
+                          <Col xs={8} className="p-2 pt-2 pb-0">
+                            <h6 className="gx tutorname">
+                              {course.host_details.first_name}{" "}
+                              {course.host_details.last_name}
+                            </h6>
+                            <p className="gl text-dark">Instructor</p>
+                          </Col>
+                          <div className="  clsfee p-2 pt-0">
+                            <div className="d-flex">
+                              <h6 className="gx">
+                                <span className="textgrey">FEE:</span> ₹
+                                {course.price}
+                                <span className="gl">/ person</span>
+                              </h6>
+                            </div>
+                            <div className="d-flex">
+                              <h6 className="gx">
+                                <span className="textgrey"> Type:</span>{" "}
+                                {course.duration_type}
+                              </h6>
+                            </div>
                           </div>
-                        </Link>
-                      </Col>
-                    );
-                  })}
-              </Row>
-         
+                        </Row>
+                      </div>
+                    </Link>
+                  </Col>
+                );
+              })}
+          </Row>
+
           <Row className=" m-0 p-0">
             <Col className="d-flex justify-content-center">
               <Link to="/classes">
