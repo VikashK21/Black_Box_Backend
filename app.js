7589c66ea6068fda4a22a128c42f32b9b59c8127
VikashK21 <vikash21@navgurukul.org>
2022-10-19 14:00:04 +0530

    the changes in the backend

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Home page//
+//Home page
 ///////Remeber for heroku APP....
 // -----> rember this part..
 if (process.env.NODE_ENV == "production") {
