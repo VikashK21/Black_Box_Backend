f04bbcc3866ebc4f804a79720a5f21be09a5864f
VikashK21 <vikash21@navgurukul.org>
2022-11-23 15:29:05 +0530

    for the heroku

--- app.js
+++ app.js
@@ -46,12 +46,12 @@ app.use(passport.session());
 //Home page///
 ///////Remeber for heroku APP....
 // -----> rember this part..
-// if (process.env.NODE_ENV == "production") {
-//   app.use(express.static("client/build"));
-//   // app.get("/*", (req, res) => {
-//   //   res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
-//   // });
-// }
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  // app.get("/*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  // });
+}
 // app.get("/", async (req, res, next) => {
 //   res.send({
 //     message:
